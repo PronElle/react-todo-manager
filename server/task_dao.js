5d41d40a777eefb9ac7f1951bfb4b406ce592ac5
PronElle <Massimiliano.Pronesti@gmail.com>
2021-05-10 22:33:17 +0200

    feat (task_dao): add complete task dao

--- server/task_dao.js
+++ server/task_dao.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const dayjs = require('dayjs');
 const sqlite = require('sqlite3').verbose();
+const Task = require('./task');
 
 const DBNAME = './tasks.db'
 
@@ -10,74 +10,103 @@ const db = new sqlite.Database(DBNAME, (err) => {
    if(err) throw err;
 });
 
-// create task
 
-// get tasks
-exports.getAll = () => {
-   return new Promise((resolve, reject) => {
-     const sql = 'SELECT * FROM tasks' ;
-     db.all(sql, [], (err, rows) => {
-       if(err)
-         reject(err);
-       else {
-         const tasks = rows.map((record) => ({id: record.id,description: record.description,important: record.important == 1,private: record.private == 1,deadline: record.deadline,completed: record.completed,user: record.user }));
-         resolve(tasks);
-       }
-     });
-   });
- };
+
+const createTaskObject = (row) => {
+  return new Task(row.id, row.description, row.important === 1, row.private === 1, row.deadline,row.completed === 1, row.user);
+}
+
+// create task
+exports.createTask = (task) => {
+  return new Promise((resolve, reject) => {
+      const query = 'INSERT INTO tasks(description, important, private, deadline, completed, user) VALUES(?,?,?, DATETIME(?),?,?)';
+      db.run(query, [task.description, task.important, task.priv, task.deadline, task.completed, task.user],  function (err) {
+          if(err)
+              reject(err);
+          else
+              resolve(this.lastID);
+      });
+  })
+}
+
+// get tasks (optionally filtered)
+exports.getTasks = (filter) => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM tasks';
+    db.all(query, [], (err, rows) => {
+      if(err)
+        reject(err);
+      else{
+        let tasks = rows.map( row => createTaskObject(row));
+        
+        switch(filter){
+          case undefined: // no filter passed
+            break;
+          case "private":
+            tasks = tasks.filter(t => t.priv);
+            break;
+          case "important":
+            tasks = tasks.filter(t => t.important);
+            break;
+          case "today":
+            tasks = tasks.filter( t => t.isToday());
+            break;
+          case "sevendays":
+            tasks = tasks.filter( t => t.isNextWeek());
+            break;
+          default:
+            tasks = []; // unknown filter
+        }
+        
+        resolve(tasks);
+      }
+    })
+
+  });
+}
 
 // get task by id
+exports.getTask = (id) => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM tasks WHERE id = ?';
+    db.all(query, [id], (err, rows) => {
+      if(err)
+        reject(err);
+      else if(rows.length === 0)
+        resolve(undefined);
+      else{
+        const task = createTaskObject(rows[0]);
+        resolve(task);
+      }
+    });
+  });
+}
 
 // delete task
+exports.deleteTask = (id) => {
+  return new Promise((resolve, reject) => {
+    const query = 'DELETE FROM tasks WHERE id = ?';
+    db.run(query, [id], (err) => {
+      if(err)
+        reject(err);
+      else
+        resolve(null);
+    })
+  });
+}
 
 // update task
-exports.updateTask = (task) => {
+exports.updateTask = (id, task) => {
    return new Promise((resolve, reject) => {
-     const sql = 'UPDATE tasks SET description=?,important=?,private=?,completed=?,user=? WHERE id = ?';
-     db.run(sql, [ task.description,task.important,task.private,task.completed,task.user, task.id], function (err) {
+     const query = 'UPDATE tasks SET description=?,important=?,private=?, deadline = DATETIME(?), completed=?,user=? WHERE id = ?';
+     // employs param id to avoid using the new task's one (might be different)
+     db.run(query, [task.description, task.important, task.priv, task.deadline, task.completed, task.user, id], function (err) {
        if (err) {
+         console.log(err);
          reject(err);
          return;
        }
        resolve(this.lastID);
      });
    });
- };
-
-
-
-
-////////////////////////////// Can be deleted if not important .
-
-// get depending on deadline
- exports.getAfterDeadline = (deadline) => {
-   return new Promise((resolve, reject) => {
-     const sql = 'SELECT * FROM tasks WHERE deadline > ?';
-     db.all(sql, [deadline.format()], (err, rows) => {
-       if(err)
-         reject(err);
-       else {
-         const tasks = rows.map((record) => (record) => ({id: record.id,description: record.description,important: record.important == 1,priv: record.private == 1,deadline: record.deadline }));
-         resolve(tasks);
-       }
-     });
-   });
- };
-
-
-
- // get depending on description
- exports.getWithWord = (word) => {
-   return new Promise((resolve, reject) => {
-     const sql = "SELECT * FROM tasks WHERE description LIKE ?";
-     db.all(sql, ["%" + word + "%"], (err, rows) => {
-       if(err)
-         reject(err);
-       else {
-         const tasks = rows.map((record) => ({id: record.id,description: record.description,important: record.important == 1,priv: record.private == 1,deadline: record.deadline }));
-         resolve(tasks);
-       }
-     });
-   });
- };
+ }
