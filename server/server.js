037718b6933bc882471e77331f9ea1126570b289
Yasser Hobballah s286584 <s286584@studenti.polito.it>
2021-05-13 14:42:36 +0300

    feat: add updateAPI

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan'); // logging middleware
 const dao = require('./task_dao.js');
+const {check, validationResult} = require('express-validator'); // validation middleware
 
 // init express
 const PORT = 3001;
@@ -32,10 +33,31 @@ app.get('/tasks', (req, res) => {
 });
 
 // PUT /tasks/:id
-app.get('/tasks/:id', (req, res) => {
-    // call updateTask here 
-});
+app.put('/tasks/:id',
+    [
+        check("priv").isBoolean(),
+        check("important").isBoolean(),
+        check("completed").isBoolean(),
+        check("description").isLength({ 'min': 1 }),
+        check("deadline").isDate({ format: 'YYYY-MM-DD', strictMode: true })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        const task = req.body;
+        const id = req.params.id;
+
+        dao.updateTask(id, task)
+            .then(() => res.status(200).end())
+            .catch((err) => res.status(503).json({
+                errors: [{ 'param': 'Server', 'msg': err }]
+            }));
+    });
+
 
 // activate server
-app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
 
