0b09732222e9e8d825689744e7a7c65e86958c7f
PronElle <Massimiliano.Pronesti@gmail.com>
2021-05-19 13:03:54 +0200

    chore: add API template

---
+++ client/api/api.js
@@ -0,0 +1,21 @@
+const baseURL = "";
+
+/* --- week 2 ---- */
+async function getTasks(filter) {
+
+}
+
+async function addTask(task){
+
+}
+
+/* --- week 3 --- */
+async function updateTask(id){
+
+}
+
+async function deleteTask(id){
+
+}
+
+export default { getTasks, addTask, updateTask, deleteTask}; 
