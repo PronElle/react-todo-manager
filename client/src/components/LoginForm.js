96a38d46511cbfc3bf64d26be5290d3d99fe83cd
PronElle <Massimiliano.Pronesti@gmail.com>
2021-06-01 23:46:47 +0200

    chore: realign tags

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -23,17 +23,17 @@ function LoginForm(props) {
 
   return (
     <>
-    <Row  className="below-nav-center" >
-      <Card border="secondary" className="mx-auto"  style={{width: '500px', height:'600px'}}>
-        <Card.Header className="bg-success">
-          <h2 className="ui teal image header mx-auto">
-                <svg className="bi bi-check-all" width="30" height="30" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M12.354 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L5 10.293l6.646-6.647a.5.5 0 01.708 0z" clipRule="evenodd"/>
-                    <path d="M6.25 8.043l-.896-.897a.5.5 0 10-.708.708l.897.896.707-.707zm1 2.414l.896.897a.5.5 0 00.708 0l7-7a.5.5 0 00-.708-.708L8.5 10.293l-.543-.543-.707.707z"/>
-                </svg>  
-                <div className="content">
-                    Log-in to ToDo Manager
-                </div>
+      <Row  className="below-nav-center" >
+        <Card border="secondary" className="mx-auto"  style={{width: '500px', height:'600px'}}>
+          <Card.Header className="bg-success">
+            <h2 className="ui teal image header mx-auto">
+              <svg className="bi bi-check-all" width="30" height="30" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M12.354 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L5 10.293l6.646-6.647a.5.5 0 01.708 0z" clipRule="evenodd"/>
+                  <path d="M6.25 8.043l-.896-.897a.5.5 0 10-.708.708l.897.896.707-.707zm1 2.414l.896.897a.5.5 0 00.708 0l7-7a.5.5 0 00-.708-.708L8.5 10.293l-.543-.543-.707.707z"/>
+              </svg>  
+              <div className="content">
+                  Log-in to ToDo Manager
+              </div>
             </h2>
           </Card.Header>
 
@@ -45,6 +45,7 @@ function LoginForm(props) {
             </svg>
             
             <Form>
+
               <Form.Group controlId='username'>
                   <InputGroup>
                     <InputGroup.Text>{iconPerson}</InputGroup.Text>
@@ -53,25 +54,23 @@ function LoginForm(props) {
               </Form.Group>
               
               <Form.Group controlId='password'>
-
                 <InputGroup>
                   <InputGroup.Text>{iconLock}</InputGroup.Text>
                   <Form.Control size="lg" type='password' value={password} placeholder="password" onChange={ev => setPassword(ev.target.value)}/>
                 </InputGroup>  
-              </Form.Group>            
+              </Form.Group> 
+
             </Form>
 
             <Row>
               <Col> <Button size="lg" variant="primary" type="submit" onClick={handleSubmit}>Login</Button></Col>
-              <Col sm={9}>{context.message && <Alert  variant='danger'>{context.message}</Alert> }</Col>
+              <Col sm={9}>{context.message && <Alert  variant='danger' onClose={() => context.setMessage('')} dismissible>{context.message}</Alert> }</Col>
             </Row>
-           
+            
           </Card.Body>
         </Card>
-        </Row>        
-      </>
- 
-    )
+      </Row>        
+    </> );
 }
 
 
